601b6190cc7853888c85768f277040250182ca16
dang03 <dang03@hotmail.com>
2014-08-13 10:48:05 +0200

    JSON data parse/loader

--- requestLoader.py
+++ requestLoader.py
@@ -5,40 +5,50 @@
 __author__ = 'Dani'
 
 import json
+import os
+import argparse
 
-with open("QoS_Request.json") as qosRequest:
-    reqData = json.load(qosRequest)
+parser = argparse.ArgumentParser(description='Suitable Path Generator')
+parser.add_argument('--controller', dest='controllerRestIp', action='store', default='localhost:8080', help='controller IP:RESTport, e.g., localhost:8080 or A.B.C.D:8080')
+args = parser.parse_args()
+print args, "\n"
+
+controllerRestIp = args.controllerRestIp
+
+
+with open("PFinput.json", 'r') as PFinput:
+    reqData = json.load(PFinput)
+    print reqData
     srcAddress = reqData['ip-src']
     dstAddress = reqData['ip-dst']
+    srcSwitch = reqData[0]['attachmentPoint'][0]['switchDPID']
+    srcPort = reqData[0]['attachmentPoint'][0]['port']
     print "QoS Request SOURCE address: %s" % srcAddress
     print "QoS Request DESTINATION address: %s" % dstAddress
+    print "SOURCE DPID: %s" % srcSwitch
+    print "SOURCE PORT: %s" % srcPort
 
+"""
 
-command = "curl -s http://%s/wm/device/?ipv4=%s" % (args.controllerRestIp, srcAddress)
-result = os.popen(command).read()
-try:
-    parsedResult = json.loads(result)
-    print command+"\n"
-
-    srcSwitch = parsedResult[0]['attachmentPoint'][0]['switchDPID']
-    srcPort = parsedResult[0]['attachmentPoint'][0]['port']
+"""
 
 
 
-
-
+"""
 command = "curl -s http://%s//wm/core/controller/switches/json" % args.controllerRestIp
 result = os.popen(command).read()
 print command+"\n"
 print result
+"""
+"""
 for parsedResult in json.loads(result):
     switchID = parsedResult['dpid']
     switchName = parsedResult['ports'][0]['name']
     print switchID
     print switchName
+"""
 
-
-    
+"""
 with open("topology.json") as topologyData:
             parsedData = json.load(topologyData)
             for i in range(len(parsedData)):
@@ -50,3 +60,4 @@ with open("topology.json") as topologyData:
                         G.add_edge(topoSrcSwitch, topoDstSwitch, key=topoSrcSwitch+topoDstSwitch, bandwidth=edgeBand)
                     print "edge: %s - %s, bandwidth: %s, key: %s" % (topoSrcSwitch, topoDstSwitch, edgeBand, topoSrcSwitch+topoDstSwitch)
 
+"""
